03159274f008bf348f38ebab26f6d896c706e99e
abdlynng <abdlynng@gmail.com>
2019-11-29 11:26:23 +0000

    activité sur le bouton et toast bien ajouté

--- helloWord/app/src/main/java/com/example/helloword/MainActivity.java
+++ helloWord/app/src/main/java/com/example/helloword/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.helloword;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.i("MainActivity","Merci d'avoir Cliquer");
+                Toast.makeText(getApplicationContext(), "Le bouton est cliqué", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
