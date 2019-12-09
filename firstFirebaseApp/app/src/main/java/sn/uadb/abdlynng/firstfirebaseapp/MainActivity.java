c72274c4071107f4bc8f5644fad2c93629e921a6
abdlynng <abdlynng@gmail.com>
2019-12-09 11:57:34 +0000

    ajout du code de read from the database dans MainActivity

--- firstFirebaseApp/app/src/main/java/sn/uadb/abdlynng/firstfirebaseapp/MainActivity.java
+++ firstFirebaseApp/app/src/main/java/sn/uadb/abdlynng/firstfirebaseapp/MainActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -41,6 +44,25 @@ public class MainActivity extends AppCompatActivity {
 
 
         myRef.setValue("Hello, World!");
+
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.i("MainActivity", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("MainActivity", "Failed to read value.", error.toException());
+            }
+        });
+
     }
 
     @Override
